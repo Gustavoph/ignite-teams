d78863f170651da8deb7bf08d31a89948fc05e36
Gustavoph <gusta.coliveira07@gmail.com>
2023-04-04 09:36:31 -0300

    feat: add navigation

--- src/screens/newGroup/index.tsx
+++ src/screens/newGroup/index.tsx
@@ -3,8 +3,21 @@ import { Container, Content, Icon } from './styles'
 import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('players', { group })
+  }
+
+  function handleChangeGroup(group: string) {
+    setGroup(group)
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,8 +29,13 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar as pessoas"
         />
 
-        <Input placeholder="Nome da Turma" style={{ marginBottom: 20 }} />
-        <Button title="Criar" />
+        <Input
+          placeholder="Nome da Turma"
+          style={{ marginBottom: 20 }}
+          onChangeText={handleChangeGroup}
+        />
+
+        <Button title="Criar" onPress={handleNewGroup} />
       </Content>
     </Container>
   )
